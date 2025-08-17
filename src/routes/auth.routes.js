97813add8cfae96a389a38c544c05df47679c5f7
Gael Renê <202408035268@alunos.unifbv.edu.br>
2025-08-17 13:14:48 -0300

    Feat: Added routes file postman example - Added instructions on readme and postman file

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { body } = require("express-validator");
 const { verifyToken } = require("@/middlewares/auth/auth-middleware");
 const AuthController = require("@/controllers/auth-controller");
-const loginLimiter = require("@/middlewares/limiters");
+const { loginLimiter } = require("@/middlewares/limiters");
 const toString = require("@/middlewares/stringfy");
 
 const router = express.Router();
